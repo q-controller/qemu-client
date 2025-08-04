dcc1d270d6ffd94f77f066facc0952df7a33729e
Nikita Vakula <nikita.vakula.dev@gmail.com>
2025-08-04 19:15:53 +0200

    macOS: use `vment-bridged` mode
    This ensures that unique IPs are leased.
    
    Signed-off-by: Nikita Vakula <programmistov.programmist@gmail.com>

--- pkg/qemu/network_darwin.go
+++ pkg/qemu/network_darwin.go
@@ -1,12 +1,37 @@
 package qemu
 
-import "fmt"
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+)
+
+func getDefaultInterface() (string, error) {
+	out, err := exec.Command("route", "get", "default").Output()
+	if err != nil {
+		return "", err
+	}
+	for _, line := range strings.Split(string(out), "\n") {
+		if strings.HasPrefix(strings.TrimSpace(line), "interface:") {
+			fields := strings.Fields(line)
+			if len(fields) == 2 {
+				return strings.TrimSpace(fields[1]), nil
+			}
+		}
+	}
+	return "", fmt.Errorf("default interface not found")
+}
 
 func build_network(id string, network NetworkConfig) ([]string, error) {
+	ifc, ifcErr := getDefaultInterface()
+	if ifcErr != nil {
+		return nil, ifcErr
+	}
+
 	args := []string{}
 
 	args = append(args, "-device", fmt.Sprintf("%s,netdev=%s,mac=%s", network.Driver, id, network.Mac))
-	args = append(args, "-netdev", fmt.Sprintf("vmnet-shared,id=%s", id))
+	args = append(args, "-netdev", fmt.Sprintf("vmnet-bridged,id=%s,ifname=%s", id, ifc))
 
 	return args, nil
 }
