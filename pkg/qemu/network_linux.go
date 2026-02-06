ccb57c874f0139ec148ec7ab186d06c61e2a02d1
Nikita Vakula <programmistov.programmist@gmail.com>
2026-02-06 12:57:37 +0100

    Add id to netdev
    If netdev is assigned an id then it is guranteed that the QMP client will
    be able to query properties of the device at path `/machine/peripheral/{id}/virtio-backend`.
    
    Signed-off-by: Nikita Vakula <programmistov.programmist@gmail.com>

--- pkg/qemu/network_linux.go
+++ pkg/qemu/network_linux.go
@@ -5,7 +5,7 @@ import "fmt"
 func build_network(id string, network NetworkConfig) ([]string, error) {
 	args := []string{}
 
-	args = append(args, "-device", fmt.Sprintf("%s,netdev=net0,mac=%s", network.Driver, network.Mac))
+	args = append(args, "-device", fmt.Sprintf("%s,netdev=net0,mac=%s,id=%s", network.Driver, network.Mac, id))
 	args = append(args, "-netdev", fmt.Sprintf("tap,id=net0,ifname=%s,script=no,downscript=no", id))
 
 	return args, nil
